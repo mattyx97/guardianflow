df01838c347f05a27ef26509b8f4ed94a3bd7a8c
Edmondo <82822454+edomod@users.noreply.github.com>
2023-12-15 14:53:53 +0100

    Refactor CardPiano component and update piani.vue

--- guardian-flow/components/CardPiano.vue
+++ guardian-flow/components/CardPiano.vue
@@ -9,9 +9,9 @@ const props = defineProps<{
 
 <template>
   <div
-    class="flex flex-col mt-[10%] md:w-full max-w-[250px] lg:max-w-[300px] py-2 bg-white group hover:text-white border border-gray-300 shadow-md rounded-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:bg-[#ED1C24]"
+    class="flex flex-col md:w-full max-w-[250px] lg:max-w-[300px] py-2 bg-white group hover:text-white border border-gray-300 shadow-md rounded-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:bg-[#ED1C24]"
   >
-    <div class="p-4 lg:p-5 text-center ">
+    <div class="p-4 lg:p-5 text-center">
       <h3 class="text-lg font-bold">{{ $props.title }}</h3>
       <div class="flex flex-col items-center text-center mt-5">
         <div class="flex flex-row items-end">
@@ -23,12 +23,13 @@ const props = defineProps<{
           <p class="font-bold">€/anno</p>
         </div>
       </div>
-      <p class=" text-gray-500 mt-5 group-hover:text-white">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
+      <p class="text-gray-500 mt-5 group-hover:text-white">
+        Il piano Small è la soluzione ideale per le piccole imprese. Con una
+        capacità di analisi fino a 300 GB di traffico di rete al mese oppure 3TB
+        annuali.
       </p>
       <a
-        class="inline-flex items-center shadow-md group-hover:text-black justify-center px-3 py-2 mt-5 text-sm font-semibold transition duration-150 rounded-lg  group-hover:bg-white text-white bg-[#ED1C24] gap-x-2 !cursor-pointer"
+        class="inline-flex items-center shadow-md group-hover:text-black justify-center px-3 py-2 mt-5 text-sm font-semibold transition duration-150 rounded-lg group-hover:bg-white text-white bg-[#ED1C24] gap-x-2 !cursor-pointer"
       >
         Acquista
       </a>
--- guardian-flow/pages/piani.vue
+++ guardian-flow/pages/piani.vue
@@ -31,10 +31,27 @@ const Piano = [
   <div class="text-center">
     <h1 class="text-3xl font-bold">I nostri piani</h1>
   </div>
-  <div class="flex flex-col gap-12 mt-3 p-5 md:flex-row justify-center items-center">
+  <div
+    class="flex flex-col gap-12 mt-[10%] p-5 md:flex-row justify-center items-center"
+  >
     <CardPiano title="Small" :priceMese="91" :priceAnno="1092" />
-    <CardPiano title="Medium" description="ciao" :priceMese="156" :priceAnno="1874" />
-    <CardPiano title="Large" description="ciao"  :priceMese="221" :priceAnno="2652"/>
-    <CardPiano title="Huge" description="ciao" :priceMese="351" :priceAnno="4212                            " />
+    <CardPiano
+      title="Medium"
+      description="ciao"
+      :priceMese="156"
+      :priceAnno="1874"
+    />
+    <CardPiano
+      title="Big"
+      description="ciao"
+      :priceMese="221"
+      :priceAnno="2652"
+    />
+    <CardPiano
+      title="Huge"
+      description="ciao"
+      :priceMese="351"
+      :priceAnno="4212"
+    />
   </div>
 </template>
