6246f5e00acbec54f464f91426f91ab129111eae
cerella17 <giuseppecerella18@gmail.com>
2023-12-27 09:36:47 +0100

    responsive checkout

--- guardian-flow/components/CardPiano.vue
+++ guardian-flow/components/CardPiano.vue
@@ -11,28 +11,28 @@ const props = defineProps<{
   <div
     class="flex flex-col md:w-full max-w-[250px] lg:max-w-[300px] py-2 bg-white group hover:text-white border border-gray-300 shadow-md rounded-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:bg-[#ED1C24]"
   >
-    <div class="p-4 lg:p-5 text-center">
+    <div class="p-4 text-center lg:p-5">
       <h3 class="text-lg font-bold">{{ $props.title }}</h3>
-      <div class="flex flex-col items-center text-center mt-5">
+      <div class="flex flex-col items-center mt-5 text-center">
         <div class="flex flex-row items-end">
-          <p class="mt-1 font-bold text-3xl">{{ $props.priceMese }}</p>
+          <p class="mt-1 text-3xl font-bold">{{ $props.priceMese }}</p>
           <p class="font-bold">€/mese</p>
         </div>
         <div class="flex flex-row items-end">
-          <p class="mt-1 font-bold text-3xl">{{ $props.priceAnno }}</p>
+          <p class="mt-1 text-3xl font-bold">{{ $props.priceAnno }}</p>
           <p class="font-bold">€/anno</p>
         </div>
       </div>
-      <p class="text-gray-500 mt-5 group-hover:text-white">
-        Il piano Small è la soluzione ideale per le piccole imprese. Con una
-        capacità di analisi fino a 300 GB di traffico di rete al mese oppure 3TB
-        annuali.
+      <p class="mt-5 text-gray-500 group-hover:text-white">
+        Il piano Small è la soluzione ideale per le piccole imprese. Con una capacità di analisi
+        fino a 300 GB di traffico di rete al mese oppure 3TB annuali.
       </p>
-      <a
+      <NuxtLink
+        to="/checkout"
         class="inline-flex items-center shadow-md group-hover:text-black justify-center px-3 py-2 mt-5 text-sm font-semibold transition duration-150 rounded-lg group-hover:bg-white text-white bg-[#ED1C24] gap-x-2 !cursor-pointer"
       >
         Acquista
-      </a>
+      </NuxtLink>
     </div>
   </div>
 </template>
--- guardian-flow/pages/checkout.vue
+++ guardian-flow/pages/checkout.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="flex flex-row  justify-center gap-12 m-5">
+  <div class="flex flex-col items-center justify-center gap-12 m-5 lg:flex-row lg:items-start">
     <div class="flex flex-col max-w-[600px]">
       <h1 class="text-2xl font-bold">Cosa include il piano [Nome Piano]</h1>
       <h1>
