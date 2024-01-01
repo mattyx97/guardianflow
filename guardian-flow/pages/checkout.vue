c5aee780cb7ae9479928342df057a7208cb97c8f
Edmondo <82822454+edomod@users.noreply.github.com>
2024-01-01 17:32:15 +0100

    Fix list checkoout page

--- guardian-flow/pages/checkout.vue
+++ guardian-flow/pages/checkout.vue
@@ -23,6 +23,57 @@ const props = defineProps<{
         </h1>
 
         <ul class="space-y-3 mt-3">
+            <li class="flex gap-2 items-center">
+            <svg
+              class="flex-shrink-0 h-4 w-4 mt-0.5 text-green-500"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <polyline points="20 6 9 17 4 12" />
+            </svg>
+            <span>Baseline personalizzata </span>
+          </li>
+            <li class="flex gap-2 items-center">
+            <svg
+              class="flex-shrink-0 h-4 w-4 mt-0.5 text-green-500"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <polyline points="20 6 9 17 4 12" />
+            </svg>
+            <span> Analisi del traffico </span>
+          </li>
+          <li class="flex gap-2 items-center">
+            <svg
+              class="flex-shrink-0 h-4 w-4 mt-0.5 text-green-500"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <polyline points="20 6 9 17 4 12" />
+            </svg>
+            <span> Notifica anomalie </span>
+          </li>
           <li class="flex gap-2 items-center">
             <svg
               class="flex-shrink-0 h-4 w-4 mt-0.5 text-green-500"
@@ -91,40 +142,7 @@ const props = defineProps<{
             </svg>
             <span> Gestione storico pagamenti </span>
           </li>
-          <li class="flex gap-2 items-center">
-            <svg
-              class="flex-shrink-0 h-4 w-4 mt-0.5 text-green-500"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <polyline points="20 6 9 17 4 12" />
-            </svg>
-            <span> Analisi del traffico </span>
-          </li>
-          <li class="flex gap-2 items-center">
-            <svg
-              class="flex-shrink-0 h-4 w-4 mt-0.5 text-green-500"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <polyline points="20 6 9 17 4 12" />
-            </svg>
-            <span> Notifica anomalie </span>
-          </li>
+
         </ul>
       </div>
     </div>
