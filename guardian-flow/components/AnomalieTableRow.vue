60486c42eb14e9c2d59d12510526f857523e1721
cerella17 <giuseppecerella18@gmail.com>
2024-01-02 16:29:12 +0100

    Add falsoPositivo functionality to AnomalieTableRow.vue

--- guardian-flow/components/AnomalieTableRow.vue
+++ guardian-flow/components/AnomalieTableRow.vue
@@ -8,10 +8,28 @@ const Anomaly = defineProps<{
   anomalyIPDest?: string;
   anomalyStatus?: boolean;
 }>();
+const falsoPositivo = ref(false);
+const showAlertConfirm = ref();
+const showAlertNotConfirm = ref();
+
+function falsoPositivoFunction() {
+  falsoPositivo.value = true;
+  if (falsoPositivo.value == true) {
+    showAlertConfirm.value = true;
+    setTimeout(() => {
+      showAlertConfirm.value = null;
+    }, 3000);
+  } else {
+    showAlertNotConfirm.value = true;
+    setTimeout(() => {
+      showAlertNotConfirm.value = null;
+    }, 3000);
+  }
+}
 </script>
 
 <template>
-  <tr class="cursor-pointer hover:bg-gray-100 ">
+  <tr class="cursor-pointer hover:bg-gray-100">
     <td class="px-6 py-4 text-sm font-medium whitespace-nowrap">{{ $props.anomalyID }}</td>
     <td class="px-6 py-4 text-sm whitespace-nowrap">{{ $props.anomalyDate }}</td>
     <td class="px-6 py-4 text-sm whitespace-nowrap">{{ $props.anomalyProtocol }}</td>
@@ -33,20 +51,85 @@ const Anomaly = defineProps<{
           class="z-50 hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-[15rem] bg-white shadow-md rounded-lg p-2 mt-2"
           aria-labelledby="hs-dropdown-custom-icon-trigger-2"
         >
-          <a
-            class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 focus:outline-none focus:bg-gray-100 hover:bg-red-500 hover:text-white"
-            href="#"
+          <button
+            @click="falsoPositivoFunction()"
+            class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 focus:outline-none focus:bg-gray-100 hover:bg-red-500 hover:text-white w-full"
           >
             Falso positivo
-          </a>
-          <a
-            class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 focus:outline-none focus:bg-gray-100 hover:bg-red-500 hover:text-white"
-            href="#"
-          >
-            Elimina
-          </a>
+          </button>
         </div>
       </div>
     </td>
   </tr>
+  <!-- alert -->
+  <div class="fixed top-0 right-0">
+    <div
+      v-if="showAlertConfirm"
+      class="p-4 bg-green-400 border-t-2 border-teal-500 rounded-lg"
+      role="alert"
+    >
+      <div class="flex">
+        <div class="">
+          <!-- Icon -->
+          <span
+            class="inline-flex items-center justify-center w-8 h-8 text-teal-800 bg-green-400 border-4 border-teal-100 rounded-full"
+          >
+            <svg
+              class="flex-shrink-0 w-4 h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" />
+              <path d="m9 12 2 2 4-4" />
+            </svg>
+          </span>
+          <!-- End Icon -->
+        </div>
+        <div class="ms-3">
+          <h3 class="font-semibold text-gray-800 dark:text-white">Successfully updated.</h3>
+          <p class="text-sm text-gray-700 dark:text-gray-400">
+            You have successfully updated your email preferences.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div v-if="showAlertNotConfirm" class="p-4 bg-red-500 border-red-500 border-s-4" role="alert">
+      <div class="flex">
+        <div class="">
+          <!-- Icon -->
+          <span
+            class="inline-flex items-center justify-center w-8 h-8 text-red-800 bg-red-200 border-4 border-red-100 rounded-full dark:border-red-900 dark:bg-red-800 dark:text-red-400"
+          >
+            <svg
+              class="flex-shrink-0 w-4 h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M18 6 6 18" />
+              <path d="m6 6 12 12" />
+            </svg>
+          </span>
+          <!-- End Icon -->
+        </div>
+        <div class="ms-3">
+          <h3 class="font-semibold text-gray-800 dark:text-white">Error!</h3>
+          <p class="text-sm text-gray-700 dark:text-gray-400">Your purchase has been declined.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
