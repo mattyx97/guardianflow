8afc5548e89c41694ca9e6ed42d77441ce2ad763
mattyx97 <mattia.guarigliq@gmail.com>
2024-01-02 11:13:59 +0100

    Update dependencies and configuration files

--- guardian-flow/nuxt.config.ts
+++ guardian-flow/nuxt.config.ts
@@ -1,7 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-icon", "@pinia/nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "nuxt-icon",
+    "@pinia/nuxt",
+    "@hebilicious/authjs-nuxt",
+  ],
+  authJs: {
+    guestRedirectTo: "/redirected",
+  },
+  nitro: {
+    routeRules: {
+      "/": { ssr: true, prerender: true },
+      "/private": { ssr: true, prerender: true },
+    },
+  },
   plugins: ["~/plugins/preline.client.ts"],
 
   components: [
@@ -12,5 +27,31 @@ export default defineNuxtConfig({
   ],
   imports: {
     dirs: ["stores", "composables/**"],
+  },
+  alias: {
+    cookie: "cookie",
+  },
+
+  // Optional default config
+  //  authJs: {
+  //    verifyClientOnEveryRequest: true,
+  //    guestRedirectTo: "/", // where to redirect if the user is not authenticated
+  //    authenticatedRedirectTo: "/", // where to redirect if the user is authenticated
+  //    baseUrl: "" // should be something like https://www.my-app.com
+  //  },
+  runtimeConfig: {
+    authJs: {
+      secret: process.env.NUXT_NEXTAUTH_SECRET, // You can generate one with `openssl rand -base64 32`
+    },
+    github: {
+      clientId: process.env.NUXT_GITHUB_CLIENT_ID,
+      clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET,
+    },
+    public: {
+      authJs: {
+        baseUrl: process.env.NUXT_NEXTAUTH_URL, // The URL of your deployed app (used for origin Check in production)
+        verifyClientOnEveryRequest: true, // whether to hit the /auth/session endpoint on every client request
+      },
+    },
   },
 });
