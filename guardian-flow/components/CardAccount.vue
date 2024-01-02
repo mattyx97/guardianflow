4940d72a9eb267c2012939ccb82bd023e0fc6a74
cerella17 <giuseppecerella18@gmail.com>
2024-01-02 17:10:22 +0100

    Refactor CardAccount.vue layout

--- guardian-flow/components/CardAccount.vue
+++ guardian-flow/components/CardAccount.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div
-    class="flex flex-col gap-5 p-4 text-black bg-white border shadow-sm rounded-xl md:p-5"
-  >
+  <div class="flex flex-col gap-5 p-4 text-black bg-white border shadow-sm rounded-xl md:p-5">
     <h3 class="text-lg font-bold text-gray-800">Il tuo account</h3>
     <div class="flex items-center justify-between gap-10 p-2">
       <div class="flex flex-col gap-[5px]">
@@ -12,14 +10,6 @@
           <h1 class="font-bold text-md">Permessi</h1>
           <h1 class="text-xs">Accesso completo</h1>
         </div>
-        <div class="flex flex-col">
-          <h1 class="font-bold text-md">Ultimo log-in</h1>
-          <h1 class="text-xs">7 Novembre 2023</h1>
-        </div>
-      </div>
-      <div class="flex flex-col">
-        <h1 class="text-4xl font-bold">33</h1>
-        <h1 class="text-xs">Login nell'ultimo mese</h1>
       </div>
     </div>
   </div>
