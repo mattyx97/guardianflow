8b606d9f149e79fbbfcdd0a7398415897028206c
Tommaso Nardi <narditommaso1@gmail.com>
2024-01-12 20:19:19 +0100

    Refactoring AI

--- ai/guardianflowai-training.py
+++ ai/guardianflowai-training.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import StandardScaler
 
+#Definizione dei campi da estrarre
 campi_desiderati = ["_source,layers,eth,eth.dst", "_source,layers,eth,eth.dst_tree,eth.addr", "_source,layers,ip,ip.src", "_source,layers,ip,ip.dst", "_source,layers,tcp,tcp.dstport", "_source,layers,tcp,tcp.flags_tree,tcp.flags.reset", "_source,layers,tcp,tcp.flags_tree,tcp.flags.syn"]
 
 def extract_features(data):
@@ -52,7 +53,6 @@ def carica_dati(nome_file):
         return []
 
 
-
 def valuta_modello(clf, X_scaled, normal_features):
     if clf is None or X_scaled is None:
         print("Modello o dati non validi per la valutazione.")
@@ -69,9 +69,9 @@ def prepara_e_addestra_modello(X):
         return None, None
 
     scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(np.array(X))
+    X_scaled = scaler.fit_transform(np.array(X))    #Standardizzazione dati
     clf = IsolationForest(contamination=0.1)
-    clf.fit(X_scaled)
+    clf.fit(X_scaled)                               #Funzione che addestra il modello su dati standardizzati
     return clf, X_scaled
 
 def main():
@@ -79,12 +79,14 @@ def main():
     nome_file_normale = "normal_traffic.json"
     nome_file_malicious = "malicious_traffic.json"
 
+    #Caricamento dati ed estrazione features
     normal_data = carica_dati(nome_file_normale)
     malicious_data = carica_dati(nome_file_malicious)
 
     normal_features = extract_features(normal_data)
     malicious_features = extract_features(malicious_data)
 
+    #Addestramento modello post-estrazione
     X = normal_features + malicious_features
     clf, X_scaled = prepara_e_addestra_modello(X)
 
